0005213fe8fee0eebd59e5a0eea7bb039dac8da4
alec resnick <aresnickety@gmail.com>
2012-12-25 12:14:56 -0500

    Added a liquid filter to strip arbitrary regexes; needed it to fix some arbitrary whitespace in the excerpt being left over (and visible in the blockquote) by the strip_html et al

---
+++ site/_plugins/strip_regex.rb
@@ -0,0 +1,13 @@
+module Jekyll
+  module StripRegex
+    def strip_regex(input, *regexes_to_strip)
+      stripped = input
+      regexes_to_strip.each do |r|
+        stripped = stripped.gsub(r.to_regexp, '')
+      end
+      return stripped
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::StripRegex)
