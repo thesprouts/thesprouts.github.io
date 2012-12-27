45c632c45b5661c0e5c16f5a70fa4a8f58c2eba7
alec resnick <aresnickety@gmail.com>
2012-12-27 11:22:55 -0500

    Setting up asset pipeline
    Installing jekyll-asset-pipeline, adding gzip, configuring for asset minifcation etc.  Would be nice to find a way to keep img in 'assets' but already spent too long fighting to control path i/o points.
    
    Signed-off-by: alec resnick <aresnickety@gmail.com>

--- site/config.rb
+++ site/config.rb
@@ -1,11 +1,10 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "css"
+css_dir = "_assets/css"
+sass_dir = "_assets/css"
 images_dir = "img"
-javascripts_dir = "js"
+javascripts_dir = "_assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
