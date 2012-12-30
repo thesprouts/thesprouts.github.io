8bd072079f3fc91e659560b4131708ef5137cbd3
alec resnick <aresnickety@gmail.com>
2012-12-29 21:54:34 -0500

    finished conversion from CSS to separate SCSS, cleaned up HTML structure a bit
    moved SCSS into a separate folder so generated CSS files weren't cluttering the tree and could easily be treated in .gitignore like _public. . .also threw in some editing of the blog styling, hr spacings, etc.
    
    Signed-off-by: alec resnick <aresnickety@gmail.com>

--- site/config.rb
+++ site/config.rb
@@ -1,8 +1,8 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-css_dir = "_assets/css/app"
-sass_dir = "_assets/css/app"
+css_dir = "_assets/css/"
+sass_dir = "_assets/scss/"
 images_dir = "img"
 javascripts_dir = "_assets/js"
 
