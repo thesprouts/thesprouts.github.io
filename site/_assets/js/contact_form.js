edad45b80bcd281321a96570fe49fbe48069994b
alec resnick <aresnickety@gmail.com>
2012-12-30 21:46:07 -0500

    fixing more validation errors
    Signed-off-by: alec resnick <aresnickety@gmail.com>

--- site/_assets/js/contact_form.js
+++ site/_assets/js/contact_form.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("form#email-us").submit(function() {
+  $("form#email-us-form").submit(function() {
     var email = $('input#message-author').val();
     var body = $('textarea#email-message').val();
 
@@ -8,10 +8,10 @@ $(function() {
         url: "/contact",
         data: "email=" + email + '&body=' + body,
         success: function() {
-          $('button#email-us').addClass("btn btn-success");
+          $('button#email-us-button').addClass("btn btn-success");
         },
         error: function() {
-          $('button#email-us').addClass("btn btn-danger");
+          $('button#email-us-button').addClass("btn btn-danger");
         }
       });
     return false;
