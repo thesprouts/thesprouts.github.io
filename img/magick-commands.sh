e0bf44def5dc80140bd08b49df9b3b9557100ac5
shaunalynn <pastelephemeral@gmail.com>
2017-10-24 13:13:16 -0400

    fixed image issues, remade sprites, added doc with poor documentation of how to make new sprite images, got rid or stripe donation button

---
+++ img/magick-commands.sh
@@ -0,0 +1,12 @@
+FOR ABOUT US IMAGES:
+magick montage alec-resnick-name.png alec-resnick.png bakhtiar-mikhak.png beth-osullivan.png john-bell.png michael-nagle-name.png michael-nagle.png molly-josephs-name.png molly-josephs.png shaunalynn-duffy-name.png shaunalynn-duffy.png -geometry 300x300 -background transparent -tile 11x1 montage.png
+
+FOR WORK IMAGES:
+magick montage byg2md.png magnetic_machines.png simulation_and_nature_in_design.png codman_ccp.png overthinking_board_games.png sprout_spaghetti_dinners.png codman_hli.png parade_floats.png steam_at_healey.png playing_math.png streetbeest.png powderhouse_studios.png turtle_geometry.png ecomodding_your_home.png quantified_self.png unlocking_locks.png engineering_the_wind.png science_of_sourdough.png flutes_and_waves.png signs_of_life.png dgmd.png -geometry 250x250 -background transparent -tile 21x1 montage.png
+
+FOR COLLABORATOR IMAGES:
+magick montage artists_in_context.png artscience.png city_of_somerville.png codman_academy.png eliot_school.png eyethink.png ginger_io.png google.png harvard_laboratory.png jaffrey_rindge.png maimonides.png mass_dot.png mit_sloan.png mit.png parts_and_crafts.png redhat.png shady_hill_school.png somerville_public_schools.png tremont_school.png tufts.png umass_boston.png xq.png -geometry 250x250 -background transparent -tile 22x1 montage.png
+
+
+
+ADD NEW IMAGES TO THE END OF THE LIST IN EACH COMMAND AND INCREASE TILE SIZE BY ONE IN ORDER TO GENERATE NEW SPRITE IMAGE THAT WONT MESS UP PREVIOUS IMAGES DISPLACEMENTS.
